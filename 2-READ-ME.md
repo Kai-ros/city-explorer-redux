e7595736840f7cf1655b7e66c1e15764d7963b75
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-07-17 11:11:05 -0700

    All feature tasks complete.

---
+++ 2-READ-ME.md
@@ -0,0 +1,15 @@
+# city-explorer-redux
+
+Front end for City Explorer app.
+
+Pair Programming Partner: Roman Gebrehiwot
+
+Number and name of feature: Components and Forms
+
+Estimate of time needed to complete: 2hrs
+
+Start time: 9:30am
+
+Finish time: 11am
+
+Actual time needed to complete: 1.5hrs
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from "react";
+
+import "./reset.css";
+import "./App.css";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>City Explorer</h1>
+        <p>
+          Enter a location below to learn about the weather, events,
+          restaurants, movies filmed there, and more!
+        </p>
+      </Fragment>
+    );
+  }
+}
+
+export default Header;
